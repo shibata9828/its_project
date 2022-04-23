936c7ec4270d5b9ed6023b28484055e2c89ff6fd
shibata9828 <m7bp67nkma@gmail.com>
2022-04-23 15:10:20 +0900

    first commit

---
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -0,0 +1,17 @@
+package com.example.its.domain.issue;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IssueService {
+
+    public List<IssueEntity> findAll(){
+        return List.of(
+                new IssueEntity(1, "概要１", "説明１"),
+                new IssueEntity(2, "概要2", "説明2"),
+                new IssueEntity(3, "概要3", "説明3")
+                );
+    }
+}
